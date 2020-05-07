211a6b4257f72ff873e024d9a9e85c02c471a71b
Bullet James <yogesh.bhardwaj@hotmail.com>
2020-05-08 03:28:24 +0530

    Lift files

---
+++ precache-manifest.9131aca80b508e22063cdc8643d5cae3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75e94c0c202a6a6f6e69b1b7d0043cbc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f00b113d6874c9907af4",
+    "url": "/static/css/main.f34a56f7.chunk.css"
+  },
+  {
+    "revision": "278a0ecf0dc09650866e",
+    "url": "/static/js/2.625798f7.chunk.js"
+  },
+  {
+    "revision": "b92b369c485a56d25a5a7495ecf46b59",
+    "url": "/static/js/2.625798f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f00b113d6874c9907af4",
+    "url": "/static/js/main.7e72ab1f.chunk.js"
+  },
+  {
+    "revision": "c3c5018b854d64cdc7f2",
+    "url": "/static/js/runtime-main.8dc0443e.js"
+  }
+]);
