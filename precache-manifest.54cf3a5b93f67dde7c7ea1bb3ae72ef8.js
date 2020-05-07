8546ef99391c8d04c40f4c10889311be234fd10b
Bullet James <yogesh.bhardwaj@hotmail.com>
2020-05-08 01:17:20 +0530

    update js

---
+++ precache-manifest.54cf3a5b93f67dde7c7ea1bb3ae72ef8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "248997b554a077cc5981e9bd1935bdf4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dfd00950ac169c31dcd8",
+    "url": "/static/css/main.f34a56f7.chunk.css"
+  },
+  {
+    "revision": "d83ad68d89d216e7bd3a",
+    "url": "/static/js/2.dcde5831.chunk.js"
+  },
+  {
+    "revision": "b92b369c485a56d25a5a7495ecf46b59",
+    "url": "/static/js/2.dcde5831.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfd00950ac169c31dcd8",
+    "url": "/static/js/main.095ce731.chunk.js"
+  },
+  {
+    "revision": "c3c5018b854d64cdc7f2",
+    "url": "/static/js/runtime-main.8dc0443e.js"
+  }
+]);
