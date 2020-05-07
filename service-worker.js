103170aaba69286bf98a2ff5d03c452bf0c2f8da
Bullet James <yogesh.bhardwaj@hotmail.com>
2020-05-08 03:27:54 +0530

    Added links, info Msgs

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.54cf3a5b93f67dde7c7ea1bb3ae72ef8.js"
+  "/precache-manifest.9131aca80b508e22063cdc8643d5cae3.js"
 );
 
 self.addEventListener('message', (event) => {
